fb7b073414e26172a9358abea1975e0236d0e53e
iamawizerd101 <bas.waanders@student.uva.nl>
2015-02-17 14:04:31 +0100

    done

--- Mr_Potato_Head/app/src/main/java/com/example/bigmac/mr_potato_head/MainActivity.java
+++ Mr_Potato_Head/app/src/main/java/com/example/bigmac/mr_potato_head/MainActivity.java
@@ -1,9 +1,16 @@
 package com.example.bigmac.mr_potato_head;
 
+import android.content.res.Resources;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.ImageView;
+import android.widget.RadioButton;
+import android.widget.Toast;
 
 
 public class MainActivity extends ActionBarActivity {
@@ -12,6 +19,7 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 
 
@@ -35,5 +43,130 @@ public class MainActivity extends ActionBarActivity {
         }
 
         return super.onOptionsItemSelected(item);
+    }
+
+    public void setPotato(ImageView imageView) {
+        ImageView Potato = (ImageView) findViewById(R.id.potatoBody);
+        Potato.setImageResource(R.drawable.body);
+
+
+    }
+
+    public void onCheckboxClicked(View view) {
+
+        boolean checked = ((CheckBox) view).isChecked();
+
+        switch (view.getId()) {
+            case R.id.checkbox_eyes:
+                if (checked) {
+                    ImageView part = (ImageView) findViewById(R.id.potatoEyes);
+                    part.setVisibility(View.VISIBLE);
+
+                } else {
+                    ImageView part = (ImageView) findViewById(R.id.potatoEyes);
+
+                    part.setVisibility(View.INVISIBLE);
+                }
+                break;
+            case R.id.checkbox_arms:
+                if (checked) {
+                    ImageView part = (ImageView) findViewById(R.id.potatoArms);
+                    part.setVisibility(View.VISIBLE);
+
+                } else {
+                    ImageView part = (ImageView) findViewById(R.id.potatoArms);
+
+                    part.setVisibility(View.INVISIBLE);
+                }
+                break;
+            case R.id.checkbox_ears:
+                if (checked) {
+                    ImageView part = (ImageView) findViewById(R.id.potatoEars);
+                    part.setVisibility(View.VISIBLE);
+
+                } else {
+                    ImageView part = (ImageView) findViewById(R.id.potatoEars);
+
+                    part.setVisibility(View.INVISIBLE);
+                }
+                break;
+            case R.id.checkbox_eyebrows:
+                if (checked) {
+                    ImageView part = (ImageView) findViewById(R.id.potatoEyebrows);
+                    part.setVisibility(View.VISIBLE);
+
+                } else {
+                    ImageView part = (ImageView) findViewById(R.id.potatoEyebrows);
+
+                    part.setVisibility(View.INVISIBLE);
+                }
+                break;
+            case R.id.checkbox_glasses:
+                if (checked) {
+                    ImageView part = (ImageView) findViewById(R.id.potatoGlasses);
+                    part.setVisibility(View.VISIBLE);
+
+                } else {
+                    ImageView part = (ImageView) findViewById(R.id.potatoGlasses);
+
+                    part.setVisibility(View.INVISIBLE);
+                }
+                break;
+            case R.id.checkbox_hat:
+                if (checked) {
+                    ImageView part = (ImageView) findViewById(R.id.potatoHat);
+                    part.setVisibility(View.VISIBLE);
+
+                } else {
+                    ImageView part = (ImageView) findViewById(R.id.potatoHat);
+
+                    part.setVisibility(View.INVISIBLE);
+                }
+                break;
+            case R.id.checkbox_mouth:
+                if (checked) {
+                    ImageView part = (ImageView) findViewById(R.id.potatoMouth);
+                    part.setVisibility(View.VISIBLE);
+
+                } else {
+                    ImageView part = (ImageView) findViewById(R.id.potatoMouth);
+
+                    part.setVisibility(View.INVISIBLE);
+                }
+                break;
+            case R.id.checkbox_mustache:
+                if (checked) {
+                    ImageView part = (ImageView) findViewById(R.id.potatoMustache);
+                    part.setVisibility(View.VISIBLE);
+
+                } else {
+                    ImageView part = (ImageView) findViewById(R.id.potatoMustache);
+
+                    part.setVisibility(View.INVISIBLE);
+                }
+                break;
+            case R.id.checkbox_nose:
+                if (checked) {
+                    ImageView part = (ImageView) findViewById(R.id.potatoNose);
+                    part.setVisibility(View.VISIBLE);
+
+                } else {
+                    ImageView part = (ImageView) findViewById(R.id.potatoNose);
+
+                    part.setVisibility(View.INVISIBLE);
+                }
+                break;
+            case R.id.checkbox_shoes:
+                if (checked) {
+                    ImageView part = (ImageView) findViewById(R.id.potatoShoes);
+                    part.setVisibility(View.VISIBLE);
+
+                } else {
+                    ImageView part = (ImageView) findViewById(R.id.potatoShoes);
+
+                    part.setVisibility(View.INVISIBLE);
+                }
+                break;
+        }
     }
 }
